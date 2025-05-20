4b808e318fb40bb692eb04099f10f75f5d8cb45f
Gary McCart <gwmccart3@gmail.com>
2025-05-20 15:58:54 -0400

    remove trial gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,8 +1,8 @@
 import './index.scss'
 import glight from '../../../assets/images/G-light.png'
 import { useEffect, useRef } from 'react'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap-trial'
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import {gsap} from 'gsap'
 
 const Logo = () => {
   const bgRef = useRef()
